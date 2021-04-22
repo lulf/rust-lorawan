65ac815d84093913582a1d3332130c77d66acdfb
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-04-22 14:30:49 +0200

    Bump receive delay

--- device/src/region/constants.rs
+++ device/src/region/constants.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 use crate::radio::{Bandwidth, CodingRate, SpreadingFactor};
 
-pub(crate) const RECEIVE_DELAY1: u32 = 1000;
+pub(crate) const RECEIVE_DELAY1: u32 = 5000;
 pub(crate) const RECEIVE_DELAY2: u32 = RECEIVE_DELAY1 + 1000; // must be RECEIVE_DELAY + 1 s
 pub(crate) const JOIN_ACCEPT_DELAY1: u32 = 5000;
 pub(crate) const JOIN_ACCEPT_DELAY2: u32 = 6000;
